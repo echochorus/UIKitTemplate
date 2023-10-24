227eb1839bb09689a262a598527934bf8621de24
Eric Williams <ewilliams.vii@gmail.com>
2023-10-24 13:29:54 -0400

    Sets up Coordinator logic. Adds .gitignore

---
+++ AppCoordinator.swift
@@ -0,0 +1,42 @@
+//
+//  AppCoordinator.swift
+//  CoordinatorTemplate
+//
+//  Created by Eric Williams on 2023-10-24.
+//
+
+import UIKit
+
+protocol Coordinator {
+    var childCoordinators: [Coordinator] { get set }
+    var navigationController: UINavigationController { get set }
+    
+    func start()
+}
+
+protocol Storyboarded {
+    static func instantiate() -> Self
+}
+
+extension Storyboarded where Self: UIViewController {
+    static func instantiate() -> Self {
+        let id = String(describing: self)
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        return storyboard.instantiateViewController(withIdentifier: id) as! Self
+    }
+}
+
+final class AppCoordinator: Coordinator {
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let vc = ViewController.instantiate()
+        navigationController.pushViewController(vc, animated: false)
+    }
+    
+}
